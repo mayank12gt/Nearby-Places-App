d1c5aa61f7270d48d7ffae0e9b4fe3a909a59346
mayank12gt <mayank.gt15@gmail.com>
2023-05-17 10:40:39 +0530

    Fetched and displayed photo of place in bottom sheet.

--- app/src/main/java/com/example/nearbyplaces/adapters/BindingAdapters.java
+++ app/src/main/java/com/example/nearbyplaces/adapters/BindingAdapters.java
@@ -14,21 +14,21 @@ import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.example.nearbyplaces.BuildConfig;
+import com.example.nearbyplaces.R;
 
 public class BindingAdapters {
 
     @BindingAdapter("imageURL")
     public static void setImage(ImageView imageView, String photo_ref){
-//        String url = "https://maps.googleapis.com/maps/api/place/photo" +
-//                "?photo_reference="+photo_ref+"&key="+ BuildConfig.MAPS_API_KEY;
-//        if(photo_ref!=null){
-//            Glide.with(imageView.getContext()).load("https://maps.googleapis.com/maps/api/place/photo?photo_reference=AZose0k5YBSCWTHz75gzKtBXlaBsUKsTdYKZ6ebi_lVei8IEKeVhzEWtpVMu1zj0xrF6dqlk_7-LHm0PQGBOHHW0gS3USrgxLdj6W69ioOUPgehH8AdDLgZbGpncV-9fvgH3XVs8Jn_CO7lF8jPuBIBI0QTygeYft-ko-dHWL35kMnjiBWON&key=AIzaSyCWW8S1nqod_hvzzIbXMICscl2tqm8wIvE")
-//                   .into(imageView);
-//
-//
-//        }
-//        else{
-//            imageView.setVisibility(View.GONE);
-//        }
+        String url = "https://maps.googleapis.com/maps/api/place/photo"+"?maxwidth=400"+
+               "&photo_reference="+photo_ref+"&key="+ BuildConfig.MAPS_API_KEY;
+        if(photo_ref!=null){
+            Glide.with(imageView.getContext()).load(url)
+                   .into(imageView);
+        }
+        else{
+            Glide.with(imageView.getContext()).load(R.drawable.placeholder)
+                    .into(imageView);
+       }
     }
 }
